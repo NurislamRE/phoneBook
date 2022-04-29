5ef3fae698054cd0cca5a23fd41cbc8076a11048
Нурислам <nurislam.raushanbek@gmail.com>
2022-04-29 17:55:48 +0600

    Sort by Asc and Desc

--- phonebook/script/script.js
+++ phonebook/script/script.js
@@ -261,22 +261,6 @@ let data = [
                 logo.textContent = text;
             });
         });
-    };
-
-    const sortByColumn = property => {                      
-        var sortOrder = 1;
-        if(property[0] === "-") {
-            sortOrder = -1;
-            property = property.substr(1);
-        }
-        return function (a,b) {
-            console.log(a);
-            /* next line works with strings and numbers, 
-             * and you may want to customize it to your needs
-             */
-            var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-            return result * sortOrder;
-        }
     };    
 
     const init = (selectorApp, title) => {
